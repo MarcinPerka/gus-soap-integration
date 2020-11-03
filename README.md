d18818de66abbc1cf24fcd7c9e3611744a573a49
MarcinPerka <marcinp1596@gmail.com>
2020-11-03 17:15:50 +0100

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# gus-soap-integration
+Project which use soap protocol to get data from GUS. 
---
+++ src/main/java/com/archu/gussoapintegration/GusSoapIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.archu.gussoapintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GusSoapIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GusSoapIntegrationApplication.class, args);
+    }
+
+}
