9e59674e52186bfebdd06e86ed50958c25914175
MarcinPerka <marcinp1596@gmail.com>
2020-11-08 09:48:51 +0100

    Add enum with reports names

---
+++ src/main/java/com/archu/gussoapintegration/integration/regon/SoapReportName.java
@@ -0,0 +1,19 @@
+package com.archu.gussoapintegration.integration.regon;
+
+public enum SoapReportName {
+    BIR11OsFizycznaDaneOgolne,
+    BIR11OsFizycznaDzialalnoscCeidg,
+    BIR11OsFizycznaDzialalnoscRolnicza,
+    BIR11OsFizycznaDzialalnoscPozostala,
+    BIR11OsFizycznaDzialalnoscSkreslonaDo20141108,
+    BIR11OsFizycznaPkd,
+    BIR11OsFizycznaListaJednLokalnych,
+    BIR11JednLokalnaOsFizycznej,
+    BIR11JednLokalnaOsFizycznejPkd,
+    BIR11OsPrawna,
+    BIR11OsPrawnaPkd,
+    BIR11OsPrawnaListaJednLokalnych,
+    BIR11JednLokalnaOsPrawnej,
+    BIR11JednLokalnaOsPrawnejPkd,
+    BIR11OsPrawnaSpCywilnaWspolnicy,
+}
