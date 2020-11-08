761c8d87efbb4af19397b5781663a8a0f9f92289
MarcinPerka <marcinp1596@gmail.com>
2020-11-08 14:41:01 +0100

    Add xsd files to resources

---
+++ src/main/java/com/archu/gussoapintegration/regon/dto/SessionDTO.java
@@ -0,0 +1,10 @@
+package com.archu.gussoapintegration.regon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SessionDTO {
+    private String sessionId;
+}
